39eb656e5079aa55178c5f3e86b4a28ad66abe49
abhishekp906 <abhishek.p906@gmail.com>
2013-08-06 17:51:30 +0530

    project resetup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smartlist::Application.config.secret_key_base = '927c998c8ea38619a6a2572fc77ac51b59f00c28fb49c1cf2cdbd7a50be7dd85a987d6d6038115a8414163ae8983557a7c1b330866dbfaaf3bb629cf5b294a33'
